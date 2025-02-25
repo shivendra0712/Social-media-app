6469f9e02b3ac4f02481976ea545adfabdced8a4
Shivendra Patel <shivendrapatel01250@gmail.com>
2025-02-25 12:19:47 +0530

    use cookieparser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const userRoutes = require('./routes/user.route')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
